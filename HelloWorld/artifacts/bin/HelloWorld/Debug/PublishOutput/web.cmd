c71ce7a444cf69c175ca3b13769b851dc7237388
gustyn <github@clanjohnson.com>
2015-09-18 12:11:54 -0400

    One more time

--- HelloWorld/artifacts/bin/HelloWorld/Debug/PublishOutput/web.cmd
+++ HelloWorld/artifacts/bin/HelloWorld/Debug/PublishOutput/web.cmd
@@ -1,2 +1,2 @@
 
-@"%~dp0approot\runtimes\dnx-coreclr-win-x64.1.0.0-beta7\bin\dnx.exe" --appbase "%~dp0approot\src\HelloWorld" Microsoft.Dnx.ApplicationHost --configuration Debug web %*
+@"%~dp0approot\runtimes\dnx-coreclr-win-x86.1.0.0-beta7\bin\dnx.exe" --appbase "%~dp0approot\src\HelloWorld" Microsoft.Dnx.ApplicationHost --configuration Debug web %*
